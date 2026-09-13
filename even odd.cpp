8e8267d46b0c74bf312fe5350c18a474d88dc829
Atharva hegade <atharvahegade32@gmail.com>
2026-09-14 01:09:55 +0530

    Create even odd.cpp

---
+++ even odd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float a;
+
+    cout<<"enter your number= ";
+    cin>>a;
+
+    if (a/2 == 0)
+    {
+        cout<<"a is even"<<endl;
+    }
+    else
+    {
+        cout<<"a is odd";
+    }
+    return 0;
+}
